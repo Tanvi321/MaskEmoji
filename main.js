b269baf734d7bda908e53b46dc3a9c7f8317a8c4
Tanvi321 <99852033+Tanvi321@users.noreply.github.com>
2022-10-31 17:47:58 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+prediction_1 = ""
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+//create your model and store it in var classifier 
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/y1c2slq4W/model.json', modelLoaded);
+//define function modelLoaded
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+//define function check() 
+function check()
+{
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+//define function gotResult(error, results)
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+
+    else
+    {
+        console.log(results);
+        document.getElementById("status").innerHTML=results[0].label;
+        if(results[0].label=="ProperMask")
+        {
+            document.getElementById("update_emoji").innerHTML="😷";
+        }
+        if(results[0].label=="NoMask")
+        {
+            document.getElementById("update_emoji").innerHTML="😔";
+        }
+        if(results[0].label=="ImproperMask")
+        {
+            document.getElementById("update_emoji").innerHTML="🤕";
+        }
+    }
+}
